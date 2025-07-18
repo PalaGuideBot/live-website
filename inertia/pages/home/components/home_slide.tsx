bbde32935c7a2880847ea334682862b6d6a43eff
riveur <riveur.cpy@gmail.com>
2025-07-18 12:47:56 +0200

    refactor: improve home slide

--- inertia/pages/home/components/home_slide.tsx
+++ inertia/pages/home/components/home_slide.tsx
@@ -1,9 +1,11 @@
-import { Card } from '@/components/ui/card'
-import { useEffect, useRef } from 'react'
-import { TwitchIcon } from 'lucide-react'
+import React, { useEffect, useRef } from 'react'
+
+import { TwitchIcon } from '@/components/icons'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { cn } from '@/lib/utils'
 import { useTwitchChat } from '../hooks/use_twitch_chat'
 
-function CustomTwitchChat() {
+function TwitchChatCard({ className, ...props }: React.ComponentProps<typeof Card>) {
   const chatContainerRef = useRef<HTMLDivElement>(null)
   const messages = useTwitchChat()
 
@@ -20,15 +22,17 @@ function CustomTwitchChat() {
   }, [messages])
 
   return (
-    <Card className="h-full shadow-lg overflow-hidden p-0">
-      <div className="flex flex-col h-full text-white">
-        <div className="bg-purple-700 p-3 text-center font-semibold">
-          Chat Twitch <TwitchIcon className="inline-block w-4 h-4 ml-1" />
-        </div>
-
+    <Card className={cn('overflow-hidden p-0 gap-0', className)} {...props}>
+      <CardHeader className="bg-gradient-to-bl from-card from-20% to-[#6441a5]/40 p-4 flex justify-center items-center">
+        <CardTitle className="flex items-center gap-1 font-semibold">
+          <span>Chat Twitch</span>
+          <TwitchIcon className="size-4 ml-1" />
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="p-4">
         <div
           ref={chatContainerRef}
-          className="flex-1 overflow-y-auto p-2 space-y-2"
+          className="flex-1 overflow-y-auto space-y-2"
           style={{
             scrollbarWidth: 'none',
             msOverflowStyle: 'none',
@@ -44,27 +48,25 @@ function CustomTwitchChat() {
             </div>
           ))}
         </div>
-      </div>
+      </CardContent>
     </Card>
   )
 }
 
 export function HomeSlide() {
   return (
-    <div className="flex justify-between w-full h-screen">
-      <div className="flex justify-start">
-        <Card className="w-full max-w-2xl p-6 shadow-lg h-1/2">
+    <div className="flex-1 grid grid-cols-4 gap-4">
+      <Card>
+        <CardContent>
           <h1 className="text-2xl font-bold mb-4">Bienvenue sur le live PalaGuideBot</h1>
-          <p className="text-gray-700">
+          <p className="text-muted-foreground">
             Sur ce live vous pouvez retrouver différentes informations concernant le serveur
             Paladium, les événements à venir, l'état des serveurs et bien plus encore. Restez
             connecté pour ne rien manquer !
           </p>
-        </Card>
-      </div>
-      <div className="w-96 h-3/4">
-        <CustomTwitchChat />
-      </div>
+        </CardContent>
+      </Card>
+      <TwitchChatCard className="col-start-4" />
     </div>
   )
 }
