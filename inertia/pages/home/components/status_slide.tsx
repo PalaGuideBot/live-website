55017703025282698e08bd186d04a5eafe47ac0f
Tony Kun <doudeville@outlook.fr>
2025-07-21 19:05:04 +0200

    refactor: remove unused accessibility layer and chart tooltip from PlayerOnlineChart

--- inertia/pages/home/components/status_slide.tsx
+++ inertia/pages/home/components/status_slide.tsx
@@ -151,7 +151,6 @@ function PlayerOnlineChart({ className, ...props }: React.ComponentProps<typeof 
       <CardContent className="flex-1 px-2 sm:p-6">
         <ChartContainer config={chartConfig} className="aspect-auto h-full w-full">
           <AreaChart
-            accessibilityLayer
             data={onlinePlayers}
             margin={{
               left: 12,
@@ -180,21 +179,6 @@ function PlayerOnlineChart({ className, ...props }: React.ComponentProps<typeof 
                   second: '2-digit',
                 })
               }}
-            />
-            <ChartTooltip
-              content={
-                <ChartTooltipContent
-                  className="w-[150px]"
-                  nameKey="count"
-                  labelFormatter={(value) => {
-                    return new Date(value).toLocaleTimeString('default', {
-                      hour: '2-digit',
-                      minute: '2-digit',
-                      second: '2-digit',
-                    })
-                  }}
-                />
-              }
             />
             <Area
               dataKey="count"
